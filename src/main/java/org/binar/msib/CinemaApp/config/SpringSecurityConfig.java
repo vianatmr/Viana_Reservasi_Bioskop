adb14e65e3136699db6dfacbaeba493a23bf81ee
Viana Tameira <190030690@stikom-bali.ac.id>
2022-10-21 13:06:50 +0700

    perbaiki plugin

--- src/main/java/org/binar/msib/CinemaApp/config/SpringSecurityConfig.java
+++ src/main/java/org/binar/msib/CinemaApp/config/SpringSecurityConfig.java
@@ -1,34 +1,27 @@
 package org.binar.msib.CinemaApp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth)
-        throws Exception{
+    public void configureGlobal (AuthenticationManagerBuilder auth) throws Exception{
         auth.inMemoryAuthentication().withUser("user")
                 .password("nununana").roles("USER");
     }
-    
-
-    protected void configure(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity.authorizeRequests()
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
                 .anyRequest().authenticated()
                 .and().formLogin()
                 .loginPage("/login").permitAll();
     }
 }
+
 
 
---
+++ src/main/java/org/binar/msib/CinemaApp/config/SwaggerConfig.java
@@ -0,0 +1,7 @@
+package org.binar.msib.CinemaApp.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+}
